854c41b8e7e83e2dea061e87dabca03718256638
Himanish15 <amiteshloll@gmail.com>
2023-11-06 13:58:25 +0400

    Daily_Challenge

---
+++ Week 2/Day_1/Daily_challenge/Daily_challenge.js
@@ -0,0 +1,14 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(fruits.splice(0, 1))
+console.log(fruits.sort())
+console.log(fruits.push("Kiwi"))
+console.log(fruits.slice(1))
+let fruit = fruits.slice(1)
+console.log(fruit.reverse())
+
+// Exercise2
+const moreFruits = ["Banana", 
+["Apples", ["Oranges"], "Blueberries"]
+];
+console.log(moreFruits[1][1])
