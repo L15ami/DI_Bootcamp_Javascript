e2e51bef05b77ed34f6a08a5bad44fb100865cf0
Himanish15 <amiteshloll@gmail.com>
2023-11-12 01:59:45 +0400

    week 2 Day 2 Exercise_Xp_Gold

--- Week 2/Day_2/Exercises_XP_GOLD/Exercises_XP_GOLD.js
+++ Week 2/Day_2/Exercises_XP_GOLD/Exercises_XP_GOLD.js
@@ -1,50 +1,50 @@
 // Exercise 1 : The World Translator
 
-// let userinput = prompt("Please enter a language : ");
-// let result = userinput.toLowerCase();
-// const language = ["french", "english", "hebrew"]
+let userinput = prompt("Please enter a language : ");
+let result = userinput.toLowerCase();
+const language = ["french", "english", "hebrew"]
 
-// switch (result){
-//     case "french":
-//         console.log("Bonjour!!!");
-//         break;
-//     case "english":
-//         console.log("Hello!!!");
-//         break;
-//     case "hebrew":
-//         console.log("Shalom!!!")
-//         break;
-//         default:
-//             console.log(`01110011 01101111 01110010 01110010 01111001`);
+switch (result){
+    case "french":
+        console.log("Bonjour!!!");
+        break;
+    case "english":
+        console.log("Hello!!!");
+        break;
+    case "hebrew":
+        console.log("Shalom!!!")
+        break;
+        default:
+            console.log(`01110011 01101111 01110010 01110010 01111001`);
 
-// }
+}
 
 // Exercise 2 : The Grade Assigner
 
-// let userinput = prompt(`Enter your grade: `)
+let userinput = prompt(`Enter your grade: `)
 
-// if(userinput > 90 ){
-//     console.log(`A`);
-// } else if (userinput >= 80 && userinput <= 90 ) {
-//     console.log(`B`);
-// } else if (userinput >= 70 && userinput <= 80){
-//     console.log(`c`);
-// } else if( userinput < 70){
-//     console.log(`D`);
-// }
+if(userinput > 90 ){
+    console.log(`A`);
+} else if (userinput >= 80 && userinput <= 90 ) {
+    console.log(`B`);
+} else if (userinput >= 70 && userinput <= 80){
+    console.log(`c`);
+} else if( userinput < 70){
+    console.log(`D`);
+}
 
 // Exercise 3 : Verbing
 
-// let userinput = prompt(`Enter a verb: `)
-// if (userinput.length >= 3 && !userinput.endsWith("ing")) {
-//     console.log(userinput + "ing");
-// } else if (userinput.length >= 3 && userinput.endsWith("ing")) {
-//     console.log(userinput + "ly");
-// } else {
-//     console.log(userinput);
-// }
+let userinput = prompt(`Enter a verb: `)
+if (userinput.length >= 3 && !userinput.endsWith("ing")) {
+    console.log(userinput + "ing");
+} else if (userinput.length >= 3 && userinput.endsWith("ing")) {
+    console.log(userinput + "ly");
+} else {
+    console.log(userinput);
+}
 
-// Exercise 4 : The “Odd Ones Out” Game
-console.log("correction")
+
+
 
 
---
+++ Week 2/Day_5/Play The Game/script.js
@@ -0,0 +1,10 @@
+function PlayTheGame() {
+    let Play = "Press a button!\nEither OK or Cancel.";
+    if (confirm(Play) === false) {
+        alert("No problem, Goodbye");
+    } else {
+        
+    }
+    
+}
+PlayTheGame();
